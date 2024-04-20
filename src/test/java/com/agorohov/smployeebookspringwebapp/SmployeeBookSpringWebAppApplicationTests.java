d4d49b1d45d69fbc8a8d9deb03a479ca97cdaac4
agorohov <aleksandr.gorohov@gmail.com>
2024-04-20 19:08:57 +0300

    initial commit

---
+++ src/test/java/com/agorohov/smployeebookspringwebapp/SmployeeBookSpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agorohov.smployeebookspringwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmployeeBookSpringWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
